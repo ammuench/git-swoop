6edfec5bb19ba97ab565064562b5132b420f1266
Alex Muench <ammuench@gmail.com>
2025-01-30 00:19:34 -0600

    📝 Build docs, makefile, etc

---
+++ README.md
@@ -0,0 +1,48 @@
+# git-swoop 🐦
+
+a quick little cli util to *swoop* to another branch, pull down the latest from remote, and then return to where you started
+
+## What it does
+
+I found myself frequently needing to jump to our develop branch and work and then return to my working branch so I could easily merge updates.  This just makes my life a bit easier, and hopefully yours too!
+
+## Install
+
+`git-swoop` can be installed by running
+
+```bash
+go install github.com/ammuench/git-swoop
+```
+
+it should then be available in your terminal as `git-swoop`
+
+Alternatively, you can clone the repo and build 
+
+if you cannot find it, you may need to configure your `GOBIN` or `GOPATH`.  [You can read the docs to configure that here](https://go.dev/ref/mod#go-install)
+
+## Commands
+
+### `git-swoop <target-branch-name>`
+
+Tries to check out your target branch, pull latest from remote, and return you to your original branch.
+
+Will try to return you to your original branch if it encounters an error.
+
+Will always log which branch you are on when it exits
+
+### `git-swoop --help # aliases -h, -help`
+
+Prints out a list of all flags and commands
+
+### `git-swoop --version # aliases -v, -version`
+
+Prints out version and license info
+
+## License
+
+`git-swoop` is published under a [GPL V3.0 License that can be found here](https://github.com/ammuench/git-swoop/LICENSE.md)
+
+## Contributing
+
+If you have anything you'd like to add, please 
+
--- main.go
+++ main.go
@@ -1,9 +1,26 @@
+// git-swoop - a quick little cli util to *swoop* to another branch, pull down the latest from remote, and then return to where you started
+// Copyright (C) 2025 Alex Muench
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package main
 
 import (
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime/debug"
 )
 
 func main() {
@@ -13,6 +30,36 @@ func main() {
 	}
 
 	swoopBranch := os.Args[1]
+
+	if swoopBranch == "-version" || swoopBranch == "--version" || swoopBranch == "-v" {
+		if info, ok := debug.ReadBuildInfo(); ok {
+			swoopVersionParsed := swoopVersion
+			if swoopVersionParsed == "" {
+				swoopVersionParsed = info.Main.Version
+			}
+			fmt.Printf("\ngit-swoop %s, built with %s\n", swoopVersionParsed, info.GoVersion)
+			fmt.Println("")
+			fmt.Println("git-swoop Copyright (C) 2025 Alex Muench")
+			fmt.Println("This program comes with ABSOLUTELY NO WARRANTY")
+			fmt.Println("This is free software, and you are welcome to redistribute it")
+			fmt.Println("under certain conditions; check the LICENSE.md file at `https://github.com/ammuench/git-swoop`")
+		}
+		return
+	}
+
+	if swoopBranch == "-h" || swoopBranch == "-help" || swoopBranch == "--help" {
+		fmt.Println("")
+		fmt.Println("usage: git-swoop <target-branch-name>")
+		fmt.Println("   git-swoop will try to checkout your target branch, pull down the latest from remote,")
+		fmt.Println("   and then return to where you started")
+		fmt.Println("")
+		fmt.Println("flags:")
+		fmt.Println("   --help (alias: -h, -help): prints this message")
+		fmt.Println("   --version (alias: -v, -version): prints the version and basic package info")
+		fmt.Println("")
+
+		return
+	}
 
 	_, err := exec.Command("git", "status").Output()
 	if err != nil {
